95534d123f80ef23efc02bd97ee0480f858836a2
foxyglue <nperdana2004@gmail.com>
2024-10-23 21:34:23 +0700

    SignUp page done

--- stockable/src/components/HeaderLogSign.jsx
+++ stockable/src/components/HeaderLogSign.jsx
@@ -3,10 +3,10 @@ import logo from "../assets/Stockable_logo.png";
 
 const HeaderLogSign = () => {
     return (
-        <header className="bg-white text-white p-4 border-2">
-        <div className="container mx-auto flex justify-between items-center pt-1">
-            <img src={logo} alt="logo" className="h-8" />
-        </div>
+        <header className="bg-white text-white p-4 border-2 w-screen">
+            <div className="container mx-auto flex justify-between items-center pt-1">
+                <img src={logo} alt="logo" className="h-8" />
+            </div>
         </header>
     );
 }
